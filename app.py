7d38b0b57da4961e9b0ded9bb2813f7967961e05
rsharma97 <rsharma97@dxc.com>
2020-05-26 23:13:47 +0530

    Baseline Code

---
+++ app.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    return "Welcome!"
+
+@app.route('/how are you')
+def hello():
+    return 'I am good, how about you?'
